b4061fdd46e251fb4ad2fb980e22e2ef502229dc
andres.mera <feli041089@gmail.com>
2023-08-10 08:10:03 -0500

    Variables con let y sus caracterisiticas

---
+++ js/01.js
@@ -0,0 +1,11 @@
+// Variables con let
+let cliente = "Juan";
+console.log("cliente", cliente);
+// una variabler conlet se puede re-asignar
+cliente = 20;
+console.log("cliente", cliente);
+cliente = true;
+console.log("cliente", cliente);
+// pueden inicar sin un valor
+let precio;
+console.log("precio", precio);
