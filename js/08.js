cc3c1951eb3e9a2f7cfb99bd7b3fa43437b3c50f
andres.mera <feli041089@gmail.com>
2023-08-10 10:24:09 -0500

    Template String

---
+++ js/08.js
@@ -0,0 +1,17 @@
+// Template Strings  o concatenacion
+
+const cliente = "Juan";
+const saldo = 400;
+console.log("El cliente " + cliente + " tiene un saldo de " + saldo);
+console.log("El cliente ", cliente, " tiene un saldo de ", saldo);
+console.log(`El cliente ${cliente} tiene un saldo de ${saldo}`);
+
+const producto = {
+  nombre: "Tablet",
+  precio: 300,
+  disponible: true,
+};
+
+console.log(
+  `El producto: ${producto.nombre}, tiene un precio de :${producto.precio}`
+);
