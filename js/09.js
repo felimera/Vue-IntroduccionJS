be606f4773801d72b1c2fedeae0b098572a4d5f4
andres.mera <feli041089@gmail.com>
2023-08-10 10:39:08 -0500

    Introduccion al arrays

---
+++ js/09.js
@@ -0,0 +1,5 @@
+// Arrays o arreglos
+const tecnologias = ["Vue.js", "JavaScript", "Node,js", 30, 20, {}];
+console.log("tecnologias", tecnologias);
+console.log("tecnologias[1]", tecnologias[1]);
+console.table("tecnologias", tecnologias);
