69d767d54a3e285456d3fb604fd804979fbe622b
andres.mera <feli041089@gmail.com>
2023-08-10 14:11:35 -0500

    Funciones con Function Declaration

---
+++ js/13.js
@@ -0,0 +1,11 @@
+// Function declaration
+
+// function sumar() {
+//   console.log(3 + 3);
+// }
+function sumar(numero1 = 0, numero2 = 10) {
+  console.log(numero1 + numero2);
+}
+sumar(1, 2);
+sumar(10, 292);
+sumar();
