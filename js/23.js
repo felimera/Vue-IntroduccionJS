44660c36d7b2489195bb598fc412455ea62f1045
andres.mera <feli041089@gmail.com>
2023-08-11 10:40:36 -0500

    Seleccionando elementos del DOM

---
+++ js/23.js
@@ -0,0 +1,13 @@
+const heading = document.querySelector(".heading");
+
+console.log("heading", heading);
+console.log("heading.textContent", heading.textContent);
+console.log("heading.innerHTML", heading.innerHTML);
+console.log("heading.classList", heading.classList);
+console.log("heading.id", heading.id);
+// heading.textContent = "Nuevo heading";
+// console.log("heading.textContent", heading.textContent);
+
+const enlaces = document.querySelectorAll(".navegacion a");
+
+console.log("enlaces", enlaces);
