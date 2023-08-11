90ab66e81b7f2ba61d6865639e2f48a05737d0d2
andres.mera <feli041089@gmail.com>
2023-08-11 09:15:32 -0500

    Scope

---
+++ js/22.js
@@ -0,0 +1,15 @@
+// Scope
+
+let precio = 300;
+
+// function unaFunction() {
+//   precio = 100;
+//   console.log(precio);
+// }
+
+function unaFunction() {
+  let precio = 100;
+  console.log(precio);
+}
+unaFunction();
+console.log(precio);
