10d77498602ee431c6abc2d7c7e5ee3ac62daa4e
andres.mera <feli041089@gmail.com>
2023-08-11 08:26:31 -0500

    Operador de comparacion

---
+++ js/18.js
@@ -0,0 +1,19 @@
+// Operador de comparacion
+
+const numero1 = 20;
+const numero2 = "20";
+
+if (numero1 == numero2) {
+  console.log("Si son iguales");
+} else {
+  console.log("No, no son iguales");
+}
+
+if (numero1 === numero2) {
+  console.log("Si son iguales");
+} else {
+  console.log("No, no son iguales");
+}
+
+console.log("typeof numero1", typeof numero1);
+console.log("typeof numero2", typeof numero2);
