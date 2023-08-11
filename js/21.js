27ea2a08c0513696a89585b1de72e43158d335ef
andres.mera <feli041089@gmail.com>
2023-08-11 09:09:49 -0500

    Optional chaining

---
+++ js/21.js
@@ -0,0 +1,11 @@
+// Optional chaining
+
+const alumno = {
+  nombre: "Juan",
+  clase: "Programacion 1",
+  aprobado: true,
+};
+
+console.log(alumno.examenes?.examen1);
+
+console.log("Hola mundo");
