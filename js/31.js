914d0f6be66e59aaa7c4bf163d9be295fdc106f9
andres.mera <feli041089@gmail.com>
2023-08-14 10:26:17 -0500

    Fetch API con Async Await

--- js/31.js
+++ js/31.js
@@ -1,1 +1,16 @@
-const url = "https://jsonplaceholder.typicode.com/comments"
+const url = "https://jsonplaceholder.typicode.com/comments";
+
+const consultarAPI = async () => {
+  console.log("desde consultarAPI");
+  try {
+    const respuesta = await fetch(url);
+    const resultado = await respuesta.json();
+    console.log("resultado", resultado);
+  } catch (error) {
+    console.log("error", error);
+  } finally {
+    console.log("Todo listo");
+  }
+};
+
+consultarAPI();
