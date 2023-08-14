f38a1023a95ebc9f96ddac4e018bd7ad77815316
andres.mera <feli041089@gmail.com>
2023-08-14 10:02:57 -0500

    Fetch API con Promises

--- js/30.js
+++ js/30.js
@@ -1,1 +1,18 @@
-const url = "https://jsonplaceholder.typicode.com/comments"
+const url = "https://jsonplaceholder.typicode.com/comments";
+
+const consultarAPI = () => {
+  console.log("desde consultarAPI");
+  fetch(url)
+    .then((respuesta) => {
+      console.log("desde el primer then");
+      console.log("respuesta", respuesta);
+      return respuesta.json();
+    })
+    .then((resultado) => {
+      console.log("resultado", resultado);
+    })
+    .catch((error) => console.log(error))
+    .finally(() => console.log("Todo listo"));
+};
+
+consultarAPI();
